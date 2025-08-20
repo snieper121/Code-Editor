ab860b4fb5f59038e16dee9ebaba54a77b74ddaa
snieper121 <mredozubovlist@gmail.com>
2025-08-21 02:32:23 +0300

    Fix: Add missing 'dp' unit import

--- app/src/main/java/com/amr/app/EditorScreen.kt
+++ app/src/main/java/com/amr/app/EditorScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.unit.dp
 import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
