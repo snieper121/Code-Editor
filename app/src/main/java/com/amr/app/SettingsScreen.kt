6b4090c9c55323756203549eb5d82a483a37d975
snieper121 <mredozubovlist@gmail.com>
2025-08-21 15:28:40 +0300

    Fix: Add screen edit

--- app/src/main/java/com/amr/app/SettingsScreen.kt
+++ app/src/main/java/com/amr/app/SettingsScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
