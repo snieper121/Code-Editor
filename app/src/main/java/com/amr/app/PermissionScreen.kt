a02aa8e462bb21ae167af7224b6013ba7036443b
snieper121 <mredozubovlist@gmail.com>
2025-08-21 10:57:16 +0300

    Fix: Add missing MaterialTheme import

--- app/src/main/java/com/amr/app/PermissionScreen.kt
+++ app/src/main/java/com/amr/app/PermissionScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.material.MaterialTheme
 import com.amr.app.theme.AppTheme
 
 @Composable
